9a89eacfed058aaadaa8e98e7e5d79d8cd6a0a6e
FridayAI <gabi.get@hotmail.com>
2022-07-03 20:21:48 -0300

    Adicionando tratamento de exceções

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        // FUNCIONARIO
         Secretaria Jorge = new Secretaria(3, 10, 1000.0, 1, "Jorge");
+        Console.WriteLine("Funcionário: ");
         Console.WriteLine($"A secretaria de id {Jorge.id} e código {Jorge.codigoFuncionario} de nome {Jorge.nome} recebe um salário de R${Jorge.valorSalario}");
 
+        // CARTÕES 
+        Console.WriteLine("\nCartões: ");
         const double taxa = 0.02;
         CartaoVA va = new CartaoVA("mastercard", 100.0, taxa);
-        Console.WriteLine($"Taxa do cartão: {va.VerificarValorTaxaCartao()}"); 
+        Console.WriteLine($"Taxa do cartão VA: {va.VerificarValorTaxaCartao()}"); 
         
         // upcasting
         Cartao cartao = va;
@@ -23,6 +27,31 @@ internal class Program
         else
         {
             Console.WriteLine("Downcasting inválido");
+        }
+
+        // TRATAMENTO EXCEÇÕES
+        Console.WriteLine("\nTratamento exceções: ");
+        Empregado Maria = new Empregado(1, "Maria"); // null pra gritar exception
+        try
+        {
+            MostrarEmpregado(Maria);
+        }
+        catch (Exception err)
+        {
+            Console.WriteLine(err.Message);
+        }
+
+
+        static void MostrarEmpregado(Empregado obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException("Empregado está nulo. Informe valores");
+            }
+            Console.WriteLine(
+                $"Nome do empregado: {obj.nome}" +
+                $"\nId do empregado: {obj.id}"
+                );
         }
     }
 }
